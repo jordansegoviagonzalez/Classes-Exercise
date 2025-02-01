6a03b50b6638491a5912f7033c6cc2cc4a473083
Jordan Segovia <segoviajordan91@gmail.com>
2025-01-31 20:52:45 -0800

    Finished creating a class named Car

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car();
+            carOne.Make = "Lamborghini";
+            carOne.Model = "Aventador";
+            carOne.Year = 2020;
+            
+            Console.WriteLine($"This is a {carOne.Make} {carOne.Model}, make in the year {carOne.Year}"); 
+            
         }
     }
 }
